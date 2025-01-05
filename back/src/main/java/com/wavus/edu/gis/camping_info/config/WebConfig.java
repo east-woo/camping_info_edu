e3c9dff7b27f9b89763c2468d24145930f61b16e
dongwoo <dwseo96@naver.com>
2025-01-06 01:50:01 +0900

    front 개발 완료

--- back/src/main/java/com/wavus/edu/gis/camping_info/config/WebConfig.java
+++ back/src/main/java/com/wavus/edu/gis/camping_info/config/WebConfig.java
@@ -6,12 +6,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         // 모든 출처에서 오는 요청을 허용
         registry.addMapping("/**")
-                .allowedOrigins("*")  // 모든 출처에서 오는 요청 허용
+                .allowedOriginPatterns("http://localhost:*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")  // 허용할 HTTP 메서드 설정
                 .allowedHeaders("*")  // 모든 헤더를 허용
                 .allowCredentials(true);  // 쿠키나 인증 정보를 요청에 포함시킬 수 있도록 허용
