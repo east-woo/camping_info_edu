8bcf1e7ad9271e55381c3e665fb57a626d9236f0
dongwoo <dwseo96@wavus.co.kr>
2025-01-06 18:04:46 +0900

    CORS 코드 변경

--- back/src/main/java/com/wavus/edu/gis/camping_info/config/WebConfig.java
+++ back/src/main/java/com/wavus/edu/gis/camping_info/config/WebConfig.java
@@ -1,18 +1,27 @@
 package com.wavus.edu.gis.camping_info.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        // 모든 출처에서 오는 요청을 허용
-        registry.addMapping("/**")
-                .allowedOriginPatterns("http://localhost:*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")  // 허용할 HTTP 메서드 설정
-                .allowedHeaders("*")  // 모든 헤더를 허용
-                .allowCredentials(true);  // 쿠키나 인증 정보를 요청에 포함시킬 수 있도록 허용
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping(("/**"))
+                        .allowedOrigins("*")
+                        .allowedMethods(
+                                HttpMethod.GET.name(),
+                                HttpMethod.HEAD.name(),
+                                HttpMethod.POST.name(),
+                                HttpMethod.PUT.name(),
+                                HttpMethod.DELETE.name());
+            }
+        };
     }
 }
